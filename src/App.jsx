6989993486c67761c8db347b73412282458e65fa
Juan Esteban <juanesteban1022@gmail.com>
2025-11-06 17:41:30 -0500

    configuración de ruta de backend desplegado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 
 import Header from './components/Header';
 import Sidebar from './pages/Sidebar';
